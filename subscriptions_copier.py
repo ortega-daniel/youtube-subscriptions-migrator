8ec8f0c612c4a7ef969d68229782c140ef5b8e5b
ortega-daniel <ic.danielortega@gmail.com>
2021-07-08 17:56:51 -0700

    get subs, get cred functions created

--- subscriptions_copier.py
+++ subscriptions_copier.py
@@ -1,68 +1,105 @@
 # Created by Daniel Ortega, July 2021
 # ic.danielortega@gmail.com
  
-
+import pprint
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
 scopes = ['https://www.googleapis.com/auth/youtube', 'https://www.googleapis.com/auth/youtube.readonly']
 api_service_name = 'youtube'
 api_version = 'v3'
-client_secrets_file = 'client_secret.json'
+client_secrets_file_path = 'client_secret.json'
 max_results = 50
 
-flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
+# returns credentials from the OAuth2.0 session
+def get_credentials(client_secrets_file, scopes, port=8080):
+    # OAuth2.0 authentication flow
+    flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
+    flow.run_local_server(port=port, prompt='consent', authorization_prompt_message='')
 
-flow.run_local_server(port=8080, prompt='consent', authorization_prompt_message='')
+    return flow.credentials
 
-# get credentials for the old channel
-credentials = flow.credentials
+# returns a list of dictionarys of the authenticated user's subsriptions
+def get_subs_auth_account(credentials):
+    youtube = build(api_service_name, api_version, credentials=credentials)
 
-youtube = build(api_service_name, api_version, credentials=credentials)
-
-# get list of subs from the old channel
-old_subs = youtube.subscriptions().list(
-    part='snippet',
-    order='alphabetical',
-    maxResults = max_results,
-    mine=True
-).execute()
-
-# first run get next page token if it exists
-try:
-    next_page_token = old_subs['nextPageToken']
-except KeyError:
-    next_page_token = None
-
-# list of dicts
-subscriptions = []
-
-# add each result to list
-for sub in old_subs['items']:
-    # subscriptions.append({channelTitle: channelId})
-    subscriptions.append({sub['snippet']['title']: sub['snippet']['resourceId']['channelId']})
-
-# get the next values on result set
-while next_page_token:
-    # request using nextPageToken
-    old_subs = youtube.subscriptions().list(
+    # get list of subs from auth user
+    subs = youtube.subscriptions().list(
         part='snippet',
         order='alphabetical',
         maxResults = max_results,
-        pageToken = next_page_token,
         mine=True
     ).execute()
 
-    # add each result to list
-    for sub in old_subs['items']:
-        # subscriptions.append({channelTitle: channelId})
-        subscriptions.append({sub['snippet']['title']: sub['snippet']['resourceId']['channelId']})
-        
-    # if there are more pages get next page token
+    # if total results = 0 return None
+    if not subs['pageInfo']['totalResults']:
+        return None
+
+    # first run get next page token if it exists
     try:
-        next_page_token = old_subs['nextPageToken']
+        next_page_token = subs['nextPageToken']
     except KeyError:
+        # key error means subs <= max_results
         next_page_token = None
     
-print(subscriptions) 
+    subscriptions = []
+
+    # add each result to list
+    for sub in subs['items']:
+        # [{channelTitle: channelId}]
+        subscriptions.append({'title': sub['snippet']['title'], 'id': sub['snippet']['resourceId']['channelId']})
+
+    # get the next values from result set
+    while next_page_token:
+        # request next {max_results} results using nextPageToken
+        subs = youtube.subscriptions().list(
+            part='snippet',
+            order='alphabetical',
+            maxResults = max_results,
+            pageToken = next_page_token,
+            mine=True
+        ).execute()
+
+        # add each result to list
+        for sub in subs['items']:
+            # [{channelTitle: channelId}]
+            subscriptions.append({'title': sub['snippet']['title'], 'id': sub['snippet']['resourceId']['channelId']})
+            
+        try:
+            # if there are more result pages get next page token
+            next_page_token = subs['nextPageToken']
+        except KeyError:
+            # key error means no more result pages
+            next_page_token = None
+
+    return subscriptions
+
+# get credentials for the old channel
+credentials = get_credentials(client_secrets_file_path, scopes)
+# get subscriptions list from the old channel
+old_subs = get_subs_auth_account(credentials)
+
+new_subs = None
+
+# if old subs list is not empty then
+if old_subs:
+    # get credentials for the new channel
+    credentials = get_credentials(client_secrets_file_path, scopes, port=8081)
+    # get subscriptions list from the new channel
+    new_subs = get_subs_auth_account(credentials)
+else:
+    print('You have no subscriptions to copy.')
+
+pprint.pprint(old_subs)
+pprint.pprint(new_subs)
+
+# insert subs from old channel into new channel
+
+
+
+
+
+
+
+
 
